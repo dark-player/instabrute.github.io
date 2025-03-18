48b0cb4f1c7c5aa58c59310e4a1b3afb2b8f63b1
lalit wagh <68015501+dark-player@users.noreply.github.com>
2025-03-18 16:05:19 +0530

    Update ighack.sh
    clean format.

--- ighack.sh
+++ ighack.sh
@@ -1,65 +1,66 @@
-#bin/bash
-#script By Infinity Trickster
-#The real creator of core files is not us so the credits 
-#Goes to real author
-#colours
-#####3#3#
+#!/bin/bash
+
+# Colors
 red='\033[1;31m'
 rset='\033[0m'
-grn='\033[1;32m'
-ylo='\033[1;33m'
-blue='\033[1;34m'
+gr='\033[1;32m'
+yl='\033[1;33m'
 cyan='\033[1;36m'
-pink='\033[1;35m'
-#coding section starts :)
+
+# Clear screen
 clear
-echo 
-echo '
+
+# ASCII banner
+cat << "EOF"
     ██╗ ██████╗       ██╗  ██╗ █████╗  ██████╗██╗  ██╗
     ██║██╔════╝       ██║  ██║██╔══██╗██╔════╝██║ ██╔╝
     ██║██║  ███╗█████╗███████║███████║██║V1.0 █████╔╝ 
     ██║██║   ██║╚════╝██╔══██║██╔══██║██║     ██╔═██╗ 
     ██║╚██████╔╝      ██║  ██║██║  ██║╚██████╗██║  ██╗
     ╚═╝ ╚═════╝       ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
-' |lolcat
-printf "\n"
-printf "                \e[101m\e[1;77m  >>  Script By roony << \e[0m\n"
-printf "\n"
-printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m youtube.com/InfinityTrickster \e[100m\e[1;77m << \e[0m\n"
-printf "\n"
-echo 
+EOF
 
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Auto Attack\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Manual Attack\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m About\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Update\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Info\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
-read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+# Menu
+printf "\n${gr}>> Script By roony <<${rset}\n"
+printf "\n${yl}1. Auto Attack\n"
+printf "2. Manual Attack\n"
+printf "3. About\n"
+printf "4. Update\n"
+printf "5. Info\n"
+printf "6. Exit\n"
 
+# Get user choice
+read -p "${cyan}[ * ] Choose an option: ${rset}" option
 
-if [[ $option == 1 || $option == 01 ]]; then
-cd $HOME/ighack/main
-bash ./auto.sh
-elif [[ $option == 2 || $option == 02 ]]; then
-cd $HOME/ighack/main
-bash ./manual.sh
-elif [[ $option == 3 || $option == 03 ]]; then
-cd $HOME/ighack/core
-bash ./about.sh
-elif [[ $option == 4 || $option == 04 ]]; then                                                                                                                
-cd $HOME/ighack/core
-bash ./update.sh
-elif [[$option == 5 || $option == 05 ]]; then
-cd $HOME/ighack/core
-bash ./info.sh
-elif [[ $option == 6 ]]; then
-exit 1
+# Handle choices
+case $option in
+    1|01)
+        cd $HOME/ighack/main
+        bash ./auto.sh
+        ;;
+    2|02)
+        cd $HOME/ighack/main
+        bash ./manual.sh
+        ;;
+    3|03)
+        cd $HOME/ighack/core
+        bash ./about.sh
+        ;;
+    4|04)
+        cd $HOME/ighack/core
+        bash ./update.sh
+        ;;
+    5|05)
+        cd $HOME/ighack/core
+        bash ./info.sh
+        ;;
+    6)
+        exit 0
+        ;;
+    *)
+        printf "\n${red}[!]> Invalid Selection!${rset}\n"
+        sleep 1
+        ;;
+esac
 
-else
-echo
-printf "\e[1;93m [>!<] Invalid Selection!\e[0m\n"
-sleep 1
-echo
-fi
-exit
+exit 0
